310db32580983f686cec46d1e07c1efd65915193
H3lpM33 <132334210+H31pm3@users.noreply.github.com>
2024-04-14 23:16:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog1.st10254714
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
